7648b8704ef085958151786420424c293600bf64
leymann <loriane.eymann@gmail.com>
2020-10-07 16:06:00 +0200

    fix: logout user if necessary when calling /connect/:provider

--- routes/connect.js
+++ routes/connect.js
@@ -39,6 +39,14 @@ module.exports = function (server) {
     oidc.stashParams,
     oidc.determineProvider,
     function (req, res, next) {
+      if (req.user) {
+        authenticator.logout(req)
+        next()
+      } else {
+        next()
+      }
+    },
+    function (req, res, next) {
       var provider = req.params.provider
       var config = settings.providers[provider]
 
